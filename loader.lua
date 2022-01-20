dce8fb5ee83e214a18ce9aa0d6667c29f3b19713
Snowy <53881173+SnowyXS@users.noreply.github.com>
2022-01-20 17:21:27 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,12 @@
 
+local placeID = game.PlaceId
+
+local success, script = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/SnowyXS/SLite/main/Games/" .. placeID .. ".lua", true) 
+end)
+
+if success then
+    print("Found script for " .. game.PlaceId)
+    
+    loadstring(script)()
+end
