ff5f045f06ed416a7085035d701f09225c6f3057
Snowy <53881173+SnowyXS@users.noreply.github.com>
2022-07-19 14:10:39 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,16 @@
-
 local placeID = game.PlaceId
+
+local repository = "https://raw.githubusercontent.com/wally-rblx/LinoriaLib/main/"
+
+local Library = loadstring(game:HttpGet(repository .. "Library.lua"))()
+local ThemeManager = loadstring(game:HttpGet(repository .. "addons/ThemeManager.lua"))()
+local SaveManager = loadstring(game:HttpGet(repository .. "addons/SaveManager.lua"))()
+
+local Window = Library:CreateWindow({
+    Title = "SLite",
+    Center = true, 
+    AutoShow = false,
+})
 
 local success, script = pcall(function()
     return game:HttpGet("https://raw.githubusercontent.com/SnowyXS/SLite/main/Games/" .. placeID .. ".lua") 
@@ -8,5 +19,33 @@ end)
 if success then
     print("Found script for " .. game.PlaceId)
     
-    loadstring(script)()
+    local GameScript = loadstring(script)()
+
+    GameScript(Window)
+
+    SaveManager:SetFolder("SLite/" .. placeID)
 end
+
+Library.Toggle()
+
+local settingsTab = Window:AddTab("Settings")
+local menuGroup = settingsTab:AddLeftGroupbox("Menu")
+
+menuGroup:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind", { 
+    Default = "End", 
+    NoUI = false, 
+    Text = "Menu keybind" 
+})
+
+Library.ToggleKeybind = Options.MenuKeybind
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings() 
+
+ThemeManager:SetFolder("SLite")
+
+SaveManager:BuildConfigSection(settingsTab) 
+
+ThemeManager:ApplyToTab(settingsTab)
