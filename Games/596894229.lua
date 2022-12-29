03602ade839554df4e48b5cacfa9f128bea4c744
Snowy <53881173+SnowyXS@users.noreply.github.com>
2022-12-29 21:56:47 +0200

    Update 596894229.lua

--- Games/596894229.lua
+++ Games/596894229.lua
@@ -749,9 +749,11 @@ return function(Window)
 
             local level = Drawing.new("Text")
             level.Text = "level: unknown"
-            level.Size = 18
+            level.Size = 24
             level.Color = Color3.new(1, 1, 1)
             level.Center = true
+            level.Outline = true
+            level.Font = 3
 
             objects.level = level
         end
