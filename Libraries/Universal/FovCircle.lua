58b436d4235f6b2be762fee931777f2b04392df9
Snowy <53881173+SnowyXS@users.noreply.github.com>
2024-11-15 01:32:35 +0200

    test fov

--- Libraries/Universal/FovCircle.lua
+++ Libraries/Universal/FovCircle.lua
@@ -32,12 +32,10 @@ function FovCircle:UpdateRadius(fov)
     local screenHeight = Camera.ViewportSize.Y
 
     local horizontalFovSize = math.tan(math.rad(fov / 2)) * screenWidth
-    local verticalFovSize = math.tan(math.rad(fov / 2)) * screenHeight
 
     local circle = self.circle
-    
-    self.fov = fov
-    circle.Radius = (horizontalFovSize + verticalFovSize) / 4
+
+    circle.Radius = horizontalFovSize
 end
 
 function FovCircle:_GetDistance(destination)
@@ -48,19 +46,11 @@ end
 
 function FovCircle:_IsInFOV(character)
     local circle = self.circle
+
     local rootPart = character.HumanoidRootPart
     local position = Camera:WorldToScreenPoint(rootPart.Position)
-
-    local screenCenter = Vector2.new(Camera.ViewportSize.X / 2, Camera.ViewportSize.Y / 2)
-
-    local dx = position.X - screenCenter.X
-    local dy = position.Y - screenCenter.Y
-    local distance = (dx^2 + dy^2)^(0.5)
-
-    local horizontalFovAngle = math.tan(math.rad(self.fov / 2)) * Camera.ViewportSize.X / 2
-    local verticalFovAngle = math.tan(math.rad(self.fov / 2)) * Camera.ViewportSize.Y / 2
-
-    return distance <= circle.Radius and math.abs(dx) <= horizontalFovAngle and math.abs(dy) <= verticalFovAngle
+    
+    return self:_GetDistance(Vector2.new(position.X, position.Y)) <= circle.Radius
 end
 
 function FovCircle:GetClosestTarget()
